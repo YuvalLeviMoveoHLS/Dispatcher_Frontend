d53ef36c8b88077dfb8d010d6adda119f4146c8a
Yuval Levi <yuvall@moveohls.com>
2023-08-31 10:41:33 +0300

    update app header

--- src/components/AppHeader/appHeader.tsx
+++ src/components/AppHeader/appHeader.tsx
@@ -5,33 +5,9 @@ import {
   StyledLogo,
 } from "./AppHeader.style";
 import LogoIcon from "../../assets/svg/logoIcon.svg";
-import SearchInput from "../Input/SearchInput";
-import FilterDropDown from "../FilterDropDown/FilterDropDown";
-import DropdownBody from "../DropdownBody/DropdownBody";
 import DropdownWithSearch from "../DropdownWithSearch/DropdownWithSearch";
 
 const AppHeader: React.FC = () => {
-  const [isDropdownVisible, setDropdownVisibility] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement>(null);
-  const toggleDropdown = () => {
-    setDropdownVisibility(!isDropdownVisible);
-  };
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      dropdownRef.current &&
-      !dropdownRef.current.contains(event.target as Node)
-    ) {
-      console.log(dropdownRef.current.contains(event.target as Node));
-
-      setDropdownVisibility(false);
-    }
-  };
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
   return (
     <AppHeaderWrapper>
       <StyledLogo src={LogoIcon} />
